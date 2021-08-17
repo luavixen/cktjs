f74858d53b9813bdf78dbfc031777a54e2876cfe
Lua ✨ <lua@foxgirl.dev>
2021-08-17 04:42:24 +0000

    Added doccomments to some variables/functions, updated package.json

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,19 @@
 export type CKTValue = null | boolean | number | string | CKTTable;
 export type CKTTable = { [index: string]: undefined | CKTValue };
+
+/**
+ * Decodes a CKT script string.
+ * @param script - Script to decode.
+ * @returns Decoded table.
+ * @throws {@link SyntaxError} on invalid syntax.
+ */
 export function parse(script: any): CKTTable;
+
+/**
+ * Encodes a value as a CKT script string.
+ * @param value - Value to encode.
+ * @param indent - Indent string/amount.
+ * @returns Encoded value.
+ * @throws {@link TypeError} on cyclic reference.
+ */
 export function stringify(value: any, indent?: string | number): string;
