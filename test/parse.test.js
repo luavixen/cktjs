1b822fdfaab03405423cb837f72febdf8c624213
Lua MacDougall <lua@foxgirl.dev>
2021-08-17 23:49:39 -0700

    Wrote tests for parse and stringify, 100% coverage!

---
+++ test/parse.test.js
@@ -0,0 +1,344 @@
+import chai from "chai";
+const { assert } = chai;
+
+import { parse } from '../lib/parse.js';
+
+const assertParse = (...cases) => {
+  for (const { input, expected } of cases) {
+    assert.deepEqual(parse(input), expected);
+  }
+};
+
+describe('stringify', () => {
+
+  it('returns an empty array for an empty document', () => {
+    assertParse({ input: '', expected: [] });
+  });
+
+  it('parses the examples from the spec', () => {
+    assertParse({
+      expected: {
+        authors: ['John Doe', 'Cricket Piapiac'],
+        database: { server: '192.0.2.62', port: 143, files: 'payroll.dat' },
+        key: 'value',
+        another: 'here is the value',
+        'unkeyed array': ['value1', 'value2'],
+        'keyed array': ['value1', 'value2'],
+        polyline: {
+          colour: 'blue',
+          thickness: 2,
+          npoints: 4,
+          0: { x:   0, y: 0 },
+          1: { x: -10, y: 0 },
+          2: { x: -10, y: 1 },
+          3: { x:   0, y: 1 },
+        },
+        key: 'this is a quoted string', // Replaced!
+        newlines: 'this is one line\nthis is another',
+        quotes: 'this string has "quotation marks" in it!',
+        'multiline string': `this string has multiple lines!\nit also has "quotation marks" in it.\nit can include # any [ characters ].`,
+        table: {
+          cat: 'meow',
+          dog: 'woof',
+        },
+      },
+      input: `
+# derived from the CKT v0.1.0 spec
+# https://git.sr.ht/~cricket/ckt/tree/f3d29afab63da85051f55643811a5d08bcf3bed4/item/SPEC
+
+# Last modified 1 April 2021 by John Doe
+authors = [ John Doe, Cricket Piapiac ]
+
+database = [
+    # Use IP address in case network name resolution isn't working
+    server = 192.0.2.62
+    port = 143
+    files = "payroll.dat"
+]
+
+key = value
+another =
+    here is the value
+
+unkeyed array = [
+  value1
+  value2
+]
+keyed array = [
+  0 = value1
+  1 = value2
+]
+
+polyline = [
+  colour = blue; thickness = 2; npoints = 4
+  [ x = 0;   y = 0 ]
+  [ x = -10; y = 0 ]
+  [ x = -10; y = 1 ]
+  [ x = 0;   y = 1 ]
+]
+
+key = this is an unquoted string
+
+"key" = "this is a quoted string"
+
+newlines = "this is one line\\nthis is another"
+quotes = "this string has \\"quotation marks\\" in it!"
+
+multiline string =
+    |this string has multiple lines!
+    |it also has "quotation marks" in it.
+    |it can include # any [ characters ].
+
+table = [
+  cat = "meow"
+  dog = "woof"
+]
+`,
+    });
+  });
+
+  it('passes the stress test', () => {
+    assertParse({
+      expected: {
+        '0': 'c',
+        '1': '123\n'
+          + '    4596    \n'
+          + '789\n'
+          + '||| | | ||| | <- this works ?? some how\n'
+          + '\n\n'
+          + ' <- literally all of the whitespace characters cktjs supports\n',
+        '2': ' these',
+        '3': ' are two different strings',
+        '4': ' as are',
+        '5': ' these',
+        '6': 'you can also nest tables!!',
+        '7': [[[['like this']]]],
+        '8': 'also!! a note on whitespace trimming',
+        '9': 'and finally!!! a cktjs-specific feature',
+        '10': 'literals that are also valid javascript literals get transformed as such',
+        ten: 10,
+        nine: 9,
+        eight: '8',
+        "'seven'": "'7'",
+        six: 6,
+        five: 5,
+        four: 4,
+        'тнгее': 'тнгее',
+        'two 🥺': ' 🥺 ',
+        "`!@$%^&*()-_+{}|':<>/? ONE": '\x00\n\r\t\x0B\f\b\x00\x1F3ÿሴ\n',
+        table: {
+          '0': 'you can put array elements in here',
+          '1': 'like this',
+          '2': 'and this',
+          '3': 'or\tthis',
+          '4': 'maybe',
+          '5': 'this!',
+          '6': 'like normal!',
+          '7': 'you can also make elements by using the next index as the key',
+          '8': "even if it's a string",
+          '9': 'or an escaped number thing !!',
+          '11': false,
+          '12': true,
+          '13': 'null',
+          '14': "which shouldn't work!! but due to the way arrays work in javascript",
+          '15': 'it ends up making an array with holes',
+          '16': 'these expressions will all be true:',
+          '17': 'table[10] === undefined',
+          '18': 'table[11] === false',
+          '19': 'table[12] === true',
+          '20': 'table[13] === null',
+          '21': 'you can also really fuck shit up',
+          'OR !! you can make keys': 'and values',
+          '(like this one) will let': 'you do weird things like...',
+          '281474976710656': 'this table will have 2^48 elements!! woah!!',
+          '281474976710657': 'and this will be the (2^48)+1th :'
+        },
+        'fucked table': [ '1', '2', '3', '8', '4' ],
+        "there's also a fancy way to write a multiline string": 'like this!!\n' +
+          'this text will be dumped verbatim\n' +
+          'even escapes!! \\x32 and \\u0000 are treated literally, not unescaped \n' +
+          '  note that cktjs does NOT add a trailing newline\n' +
+          '  so this newline will be IGNORED ->',
+        and: {
+          '0': 'you can nest them',
+          '1': { '0': ['to infinity'], 'keep em going': 'weeeee' },
+          'like this too': [
+            'and just!! keep going',
+            'to infinity',
+            'or until the stack runs out'
+          ]
+        },
+        'this will have no whitespace around it': 'this will have no whitespace around it',
+        ' \t\tbut this will!!\f': ' \t\tbut this will!!\f',
+        literals: [
+          'null',       'true',
+          'false',      '1',
+          '10',         '-10',
+          '+10',        '10e32',
+          '10E31',      '3.4',
+          '-3.443e+12', '-0090.090E-2',
+          "'null'",     'true',
+          'fAlse',      '+-10',
+          '10:3',       '1ee4',
+          '*3',         "'10'"
+        ]
+      },
+      input: `
+# comment
+# another comment
+
+ten = 10
+nine=9
+"eight"="8"
+'seven'  =  '7',six =
+6
+  five
+
+# let's see you try and parse this!!\x20\x20
+
+=#3 ... just kidding :)
+
+    5;\\x66\\x6F\\x75\\x72=\\x34 # this should parse as {four: 4}
+\\u0442\\u043d\\u0433\\u0435\\u0435
+=
+"\\u0442\\u043d\\u0433\\u0435\\u0435" # "тнгее" cyrillic script
+
+ two \\ud83E\\uDd7A  =  " \\ud83E\\uDd7A "
+ #   ^ pleading emoji :3
+
+ # \/ every special key on my kb
+ \`!@$%^&*()-_+{}\\|':<>/? ONE = \\0\\n\\r\\t\\v\\f\\b\\x00\\x1F\\x33\\xFF\\u1234\\n
+
+table = [
+  you can put array elements in here
+  like this\x20
+  and this;\tor\tthis\x20\x20
+  , maybe ;   this!
+  OR !! you can make keys = and values, like normal!
+  7 = you can also make elements by using the next index as the key
+  "8" = even if it's a string;\\x39=or an escaped number thing !!
+  # some implementations
+  (like this one) will let = you do weird things like...
+  12 = true
+  11 = false
+  null
+  which shouldn't work!! but due to the way arrays work in javascript,
+  it ends up making an array with holes,
+  these expressions will all be true:
+  "table[10] === undefined"
+  "table[11] === false"
+  "table[12] === true"
+  "table[13] === null"
+  you can also really fuck shit up
+  281474976710656 = this table will have 2^48 elements!! woah!!
+  and this will be the (2^48)+1th :]c
+
+fucked table=[;,;,;,;,;;;,;;
+
+;,;;, ,; ,; ,;;,;;, ,; ;1 ,; ;2;,3;,,;;,;,;,;,;,\x20
+
+\t\t\v\t \f\r8\v\f\r; \x20
+4;,
+,,;\x20\x20
+
+;;;;;;;;;;;;;;;]
+
+there's also a fancy way to write a multiline string =
+  |like this!!
+  |this text will be dumped verbatim
+  |even escapes!! \\x32 and \\u0000 are treated literally, not unescaped\x20
+  |  note that cktjs does NOT add a trailing newline
+  |  so this newline will be IGNORED ->
+
+|123
+  |    4596   \x20
+                                      |789
+|||| | | ||| | <- this works ?? some how
+|
+   |
+   \t\v\f\r | <- literally all of the whitespace characters cktjs supports
+|
+# ^ trailing newline
+
+ | these
+,| are two different strings
+
+| as are
+
+         | these
+
+you can also nest tables!!
+
+[[[[like this]]]]
+
+and = [
+  you can nest them
+  like this too = [
+    and just!! keep going,
+    to infinity
+    or until the stack runs out;
+  ];[
+    keep em going = weeeee,[  to infinity    ]
+  ]
+  #[][]
+  #^ invalid btw
+]
+
+also!! a note on whitespace trimming
+  \r   \t this will have no whitespace around it \t\t   \v \f = "this will have no whitespace around it"
+  \\x20\t\tbut this will!!\\f = " \t\tbut this will!!\f"
+
+and finally!!! a cktjs-specific feature,
+literals that are also valid javascript literals get transformed as such
+
+literals = [
+  null
+  true
+  false
+  1
+  10
+  -10
+  +10
+  10e32
+  10E31
+  3.4
+  -3.443e+12
+  -0090.090E-2
+  # these are not valid, however
+  'null'
+  "true"
+  fAlse
+  +-10
+  10:3
+  1ee4
+  *3
+  '10'
+  # etc...
+]
+
+`,
+    });
+  });
+
+  it('redefines properties that clash with Object.prototype', () => {
+    const object = parse('hasOwnProperty=all of these,__defineGetter__=should get,propertyIsEnumerable=redefined');
+    assert.strictEqual(object.hasOwnProperty, 'all of these');
+    assert.strictEqual(object.__defineGetter__, 'should get');
+    assert.strictEqual(object.propertyIsEnumerable, 'redefined');
+  });
+
+  it('ignores __proto__', () => {
+    const object = parse('__proto__="prototype pollution?"');
+    assert.strictEqual(Object.getOwnPropertyDescriptor(object, '__proto__'), undefined);
+  });
+
+  it('generates errors for invalid syntax', () => {
+    assert.throws(() => parse(`\\`), SyntaxError, 'CKT 1:1 unexpected character');
+    assert.throws(() => parse(`\\x`), SyntaxError, 'CKT 1:3 incomplete hex escape');
+    assert.throws(() => parse(`\\u`), SyntaxError, 'CKT 1:3 incomplete hex escape');
+    assert.throws(() => parse(`=`), SyntaxError, 'CKT 1:1 expected value');
+    assert.throws(() => parse(`[`), SyntaxError, 'CKT 1:2 expected value');
+    assert.throws(() => parse(`|\n=`), SyntaxError, 'CKT 4:2 unexpected =');
+  });
+
+});
---
+++ test/stringify.test.js
@@ -0,0 +1,287 @@
+import chai from "chai";
+const { assert } = chai;
+
+import { stringify } from '../lib/stringify.js';
+
+const assertStringify = (...cases) => {
+  for (const { input, indent, expected } of cases) {
+    assert.strictEqual(stringify(input, indent), expected);
+  }
+};
+
+describe('stringify', () => {
+
+  it('stringifies JSON primitives', () => {
+    assertStringify(
+      { input: null, expected: `null` },
+      { input: false, expected: `false` },
+      { input: true, expected: `true` },
+      { input: 0, expected: `0` },
+      { input: 1, expected: `1` },
+      { input: 10, expected: `10` },
+      { input: 1.0, expected: `1` },
+      { input: 1.1, expected: `"1.1"` },
+      { input: 1.01, expected: `"1.01"` },
+      { input: 32, expected: `32` },
+      { input: -98.44, expected: `"-98.44"` },
+      { input: 1e33, expected: `"1e+33"` },
+      { input: 6.0221409e+23, expected: `"6.0221409e+23"` },
+      { input: -2.99792458e-6, expected: `"-0.00000299792458"` },
+      { input: -9.6574e-99, expected: `"-9.6574e-99"` },
+      { input: '', expected: `""` },
+      { input: ' ', expected: `" "` },
+      { input: 'hello', expected: `hello` },
+      { input: 'hello world', expected: `hello world` },
+      { input: 'hello, world', expected: `"hello, world"` },
+      { input: 'hello_world', expected: `hello_world` },
+      { input: 'hello-world', expected: `"hello-world"` },
+      { input: '1000 Cool Strings', expected: `1000 Cool Strings` },
+      { input: '1000 Cool Strings!', expected: `"1000 Cool Strings!"` },
+      { input: '1000! Cool Strings', expected: `"1000! Cool Strings"` },
+      { input: '\uD83E\uDD7A', expected: `"\\ud83e\\udd7a"` },
+      { input: '\x00\x1F\x7F', expected: `"\\x00\\x1f\\u007f"` },
+      { input: '\x18weird\x1Aascii\x0C\x0Dcharacters\xFF\x1B', expected: `"\\x18weird\\x1aascii\\x0c\\rcharacters\\u00ff\\x1b"` },
+    );
+  });
+
+  it('stringifies a simple object with nested objects and arrays', () => {
+    const object = {
+      a: 10,
+      b: 20,
+      c: 30,
+      objectKey: 'stringValue',
+      nestedObject: {
+        x: null, y: false, z: true,
+      },
+      array: [
+        'first element', 'second element', 'third element',
+        { this: 'is the 4th element', name: { first: 'CKT', last: '???' } },
+      ],
+    };
+
+    assertStringify({
+      input: object,
+      indent: 2,
+      expected:
+`a = 10
+b = 20
+c = 30
+objectKey = stringValue
+nestedObject = [
+  x = null
+  y = false
+  z = true
+]
+array = [
+  first element
+  second element
+  third element
+  [
+    this = is the 4th element
+    name = [
+      first = CKT
+      last = "???"
+    ]
+  ]
+]`,
+    });
+
+    assertStringify({
+      input: object,
+      expected: `a=10,b=20,c=30,objectKey=stringValue,nestedObject=[x=null,y=false,z=true],array=[first element,second element,third element,[this=is the 4th element,name=[first=CKT,last="???"]]]`,
+    });
+  });
+
+  it('omits unsupported values', () => {
+    assertStringify({
+      input: {
+        functions: [function () {}, (a, b, c) => a],
+        symbols: [Symbol(), Symbol('test'), Symbol.iterator],
+        undefined: [undefined, void 0],
+        bigints: [0n, 10n, -32n, 10000000000000n],
+        others: [null, false, true, 0, 1, '', 'hello'],
+        objectOmitTest: {
+          omit1: Symbol(),
+          omit2: undefined,
+          omit3: () => {},
+          omit4: 100n,
+          keep1: null,
+          keep2: 40,
+          keep3: 'kept!',
+        },
+      },
+      expected: `functions=[],symbols=[],undefined=[],bigints=[],others=[null,false,true,0,1,"",hello],objectOmitTest=[keep1=null,keep2=40,keep3="kept!"]`,
+    });
+    assertStringify(
+      { input: function () {}, expected: `` },
+      { input: Symbol(), expected: `` },
+      { input: undefined, expected: `` },
+      { input: 2048n, expected: `` },
+    );
+  });
+
+  it('unboxes boxed primitives', () => {
+    assertStringify(
+      { input: new Boolean(false), expected: `false` },
+      { input: new Boolean(true), expected: `true` },
+      { input: new Number(0), expected: `0` },
+      { input: new Number(-10.44), expected: `"-10.44"` },
+      { input: new String(''), expected: `""` },
+      { input: new String('hello'), expected: `hello` },
+      {
+        input: {
+          a: new Boolean(true),
+          b: new Number(1024),
+          c: new String('test string!!'),
+        },
+        expected: `a=true,b=1024,c="test string!!"`,
+      },
+    );
+  });
+
+  it('respects toJSON', () => {
+    assertStringify(
+      { input: new Date(1629263887685), expected: `"2021-08-18T05:18:07.685Z"` },
+      { input: { toJSON: () => [['yes']] }, expected: `[yes]` },
+      {
+        input: {
+          a: { toJSON: () => [1, 2, 3, { four: 4 }] },
+        },
+        expected: `a=[1,2,3,[four=4]]`,
+      },
+    );
+  });
+
+  it('replaces invalid numbers with null', () => {
+    assertStringify(
+      { input: NaN, expected: `null` },
+      { input: Infinity, expected: `null` },
+      { input: -Infinity, expected: `null` },
+    );
+  });
+
+  it('detects cyclic references', () => {
+    const x = { z: null };
+    const y = { x: x };
+    const z = { y: y };
+    x.z = z;
+    const array = [1, { two: 2 }, 3, 4, {}, 6, 7, { a: 8 }];
+    const object = {
+      date: new Date(),
+      nested: {
+        thisIsFalse: false,
+        thisIsTrue: true,
+        values: [[array]],
+      },
+    };
+    array[3] = object;
+    assert.throws(() => stringify(x), TypeError, 'CKT: cyclic reference');
+    assert.throws(() => stringify(object), TypeError, 'CKT: cyclic reference');
+  });
+
+  it('handles mixed array/key-value tables', () => {
+    const table = [0, 1, 2, 3, 4];
+    table[5] = 5;
+    table.bird = 'chirp';
+    table[6] = 6;
+    table[7] = 7;
+    table[10] = 10;
+    table.dog = 'woof';
+    table.cat = 'meow';
+    table[11] = 11;
+    table[12] = 12;
+    table[4938] = 4938;
+    assertStringify({
+      input: { table },
+      expected: `table=[0,1,2,3,4,5,6,7,10=10,11=11,12=12,4938=4938,bird=chirp,dog=woof,cat=meow]`,
+    });
+    assertStringify({
+      input: {
+        0: 'zero',
+        1: 'one',
+        2: 'two',
+        4: 'four',
+        3: 'three', // Will be re-ordered due to the way object literals work
+        numbers: 'yes',
+        6: 'six',
+      },
+      expected: `zero,one,two,three,four,6=six,numbers=yes`,
+    });
+  });
+
+  it('pretty prints using provided indent', () => {
+    const document = {
+      0: 'hello world',
+      1: 'goodbye world',
+      array: [
+        10, 20, 30,
+      ],
+      object: {
+        a: 10, b: 20, c: 30,
+      },
+      nested: [[[[['hello there!!! i\'m very indented :3']]]]],
+    };
+    assertStringify(
+      {
+        input: document,
+        indent: false, // Ignored if not number or string,
+        expected: `hello world,goodbye world,array=[10,20,30],object=[a=10,b=20,c=30],nested=[[[[["hello there!!! i'm very indented :3"]]]]]`,
+      },
+      {
+        input: document,
+        indent: 4, // 4 spaces
+        expected: `hello world
+goodbye world
+array = [
+    10
+    20
+    30
+]
+object = [
+    a = 10
+    b = 20
+    c = 30
+]
+nested = [
+    [
+        [
+            [
+                [
+                    "hello there!!! i'm very indented :3"
+                ]
+            ]
+        ]
+    ]
+]`,
+      },
+      {
+        input: document,
+        indent: '\t\t\t', // 3 tabs
+        expected: `hello world
+goodbye world
+array = [
+\t\t\t10
+\t\t\t20
+\t\t\t30
+]
+object = [
+\t\t\ta = 10
+\t\t\tb = 20
+\t\t\tc = 30
+]
+nested = [
+\t\t\t[
+\t\t\t\t\t\t[
+\t\t\t\t\t\t\t\t\t[
+\t\t\t\t\t\t\t\t\t\t\t\t[
+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t"hello there!!! i'm very indented :3"
+\t\t\t\t\t\t\t\t\t\t\t\t]
+\t\t\t\t\t\t\t\t\t]
+\t\t\t\t\t\t]
+\t\t\t]
+]`,
+      }
+    );
+  });
+
+});
