aad2e7fcb2f3fc17db5808a9b9a2f06edfc1d06d
Lua MacDougall <lua@foxgirl.dev>
2021-08-17 21:00:33 -0700

    Refactored entire library with cleaner code and using a new parsing system

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,18 +2,18 @@ export type CKTValue = null | boolean | number | string | CKTTable;
 export type CKTTable = { [index: string]: undefined | CKTValue };
 
 /**
- * Decodes a CKT script string.
- * @param script - Script to decode.
- * @returns Decoded table.
- * @throws {@link SyntaxError} on invalid syntax.
+ * Decodes a CKT script string
+ * @param script - Script to decode
+ * @returns Decoded table
+ * @throws {@link SyntaxError} on invalid syntax
  */
 export function parse(script: any): CKTTable;
 
 /**
- * Encodes a value as a CKT script string.
- * @param value - Value to encode.
- * @param indent - Indent string/amount.
- * @returns Encoded value.
- * @throws {@link TypeError} on cyclic reference.
+ * Encodes a value as a CKT script string
+ * @param value - Value to encode
+ * @param indent - Indent string/amount
+ * @returns Encoded value
+ * @throws {@link TypeError} on cyclic reference
  */
 export function stringify(value: any, indent?: string | number): string;
