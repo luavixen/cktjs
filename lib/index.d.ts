ce06b40990fba106a94d5bb9a6efaf5e4b1f012d
Lua MacDougall <lua@foxgirl.dev>
2021-08-21 14:26:52 -0700

    Updated type definitions w/ new CKTArray and updated doccomments

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,19 +1,20 @@
-export type CKTValue = null | boolean | number | string | CKTTable;
-export type CKTTable = { [index: string]: undefined | CKTValue };
+export type CKTValue = null | boolean | number | string | CKTArray | CKTTable;
+export type CKTArray = CKTValue[];
+export type CKTTable = { [key: string]: CKTValue };
 
 /**
- * Decodes a CKT script string
- * @param script - Script to decode
- * @returns Decoded table
+ * Parses a string as a CKT document
+ * @param text - Text to parse
+ * @returns Decoded CKT table
  * @throws {@link SyntaxError} on invalid syntax
  */
-export function parse(script: any): CKTTable;
+export function parse(text: any): CKTTable;
 
 /**
- * Encodes a value as a CKT script string
+ * Encodes a value as a CKT document
  * @param value - Value to encode
- * @param indent - Indent string/amount
- * @returns Encoded value
+ * @param indent - Indent string/space count
+ * @returns Encoded CKT document
  * @throws {@link TypeError} on cyclic reference
  */
 export function stringify(value: any, indent?: string | number): string;
