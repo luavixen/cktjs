9bf693c6bc7b98b2aab3b9e5c60588c7298a6767
Lua MacDougall <lua@foxgirl.dev>
2022-01-03 13:40:15 -0800

    Updated Deno exports, added mod.ts, npmignored Deno exports

--- cktjs.ts
+++ cktjs.ts
@@ -1,1 +1,2 @@
-export * from './lib/index.js';
+// This module re-exports mod.ts to be backwards-compatible with cktvs@1.0.4
+export * from './mod.ts';
---
+++ mod.ts
@@ -0,0 +1,2 @@
+export { parse, stringify } from './lib/index.js';
+export type { CKTValue, CKTArray, CKTTable } from './lib/index.js';
